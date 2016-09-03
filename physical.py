4564676c4846c236f5bc951ec3cc8294d5f0884f
MOOOWOOO <mooowooo@live.com>
2016-09-03 23:36:04 +0800

    [Feature](physical): complete the lock unlock seek func

--- physical.py
+++ physical.py
@@ -1,23 +1,37 @@
 # coding: utf-8
+import os
+
+from portalocker import portalocker
 
 __author__ = 'Jux.Liu'
 
 class Storage(object):
     def __init__(self, dbfile):
-        self._db = dbfile
+        self.dbfile = dbfile
         self.locked = False
+
+        self.superblock_size = 4096
+        self._ensure_superblock()
 
     def lock(self):
         if self.locked:
-            pass
+            return False
+        else:
+            portalocker.lock(self.dbfile, portalocker.LOCK_EX)
+            self.locked = True
+            return True
 
     def unlock(self):
-        pass
+        if self.locked:
+            self.dbfile.flush()
+            portalocker.unlock(self.dbfile)
+            self.locked = False
+            return True
+        else:
+            return False
 
     def _seek_end(self):
-        pass
-
-    superblock_size = 4096
+        self.dbfile.seek(0, os.SEEK_END)
 
     def _ensure_superblock(self):
         '''
@@ -26,11 +40,19 @@ class Storage(object):
         '''
         self.lock()
         self._seek_end()
-        end_address = self._db.tell()
-        space = self.superblock_size - end_address
+        end_address = self.dbfile.tell()
+        empty_space = self.superblock_size - end_address
         # full-fill the empty space of superblock
-        if space > 0:
-            self._db.write(b'\x00' * space)
+        if empty_space > 0:
+            self.dbfile.write(b'\x00' * empty_space)
         else:
             pass
         self.unlock()
+
+    @property
+    def closed(self):
+        return self.dbfile.closed
+
+    def close(self):
+        self.unlock()
+        self.dbfile.close()
